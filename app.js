5a4567f541845c25c9fe6ed563522745f714bbd6
Ghibbi Moure <gmoure@berklee.edu>
2024-05-09 03:53:24 -0400

    PROJECT HAHAHA

--- app.js
+++ app.js
@@ -1,92 +1,64 @@
-var Quiz = function(){
-    var self = this;
-    this.init = function(){
-      self._bindEvents();
-    }
-    
-    this.correctAnswers = [
-      { question: 1, answer: 'a' },
-      { question: 2, answer: 'b' },
-      { question: 3, answer: 'd' },
-      { question: 4, answer: 'c' },
-      { question: 5, answer: 'd' },
-      { question: 6, answer: 'b' },
-    ]
-    
-    this._pickAnswer = function($answer, $answers){
-      $answers.find('.quiz-answer').removeClass('active');
-      $answer.addClass('active');
-    }
-    this._calcResult = function(){
-      var numberOfCorrectAnswers = 0;
-      $('ul[data-quiz-question]').each(function(i){
-        var $this = $(this),
-            chosenAnswer = $this.find('.quiz-answer.active').data('quiz-answer'),
-            correctAnswer;
-        
-        for ( var j = 0; j < self.correctAnswers.length; j++ ) {
-          var a = self.correctAnswers[j];
-          if ( a.question == $this.data('quiz-question') ) {
-            correctAnswer = a.answer;
-          }
-        }
-        
-        if ( chosenAnswer == correctAnswer ) {
-          numberOfCorrectAnswers++;
-          
-          // highlight this as correct answer
-          $this.find('.quiz-answer.active').addClass('correct');
-        }
-        else {
-          $this.find('.quiz-answer[data-quiz-answer="'+correctAnswer+'"]').addClass('correct');
-          $this.find('.quiz-answer.active').addClass('incorrect');
-        }
-      });
-      if ( numberOfCorrectAnswers < 3 ) {
-        return {code: 'bad', text: 'Poor spelling skills'};
-      }
-      else if ( numberOfCorrectAnswers == 3 || numberOfCorrectAnswers == 4 ) {
-        return {code: 'mid', text: 'Moderate spelling skills'};
-      }
-      else if ( numberOfCorrectAnswers > 4 ) {
-        return {code: 'good', text: 'Good spelling skills'};
-      }
-    }
-    this._isComplete = function(){
-      var answersComplete = 0;
-      $('ul[data-quiz-question]').each(function(){
-        if ( $(this).find('.quiz-answer.active').length ) {
-          answersComplete++;
-        }
-      });
-      if ( answersComplete >= 6 ) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
-    this._showResult = function(result){
-      $('.quiz-result').addClass(result.code).html(result.text);
-    }
-    this._bindEvents = function(){
-      $('.quiz-answer').on('click', function(){
-        var $this = $(this),
-            $answers = $this.closest('ul[data-quiz-question]');
-        self._pickAnswer($this, $answers);
-        if ( self._isComplete() ) {
-          
-          // scroll to answer section
-          $('html, body').animate({
-            scrollTop: $('.quiz-result').offset().top
-          });
-          
-          self._showResult( self._calcResult() );
-          $('.quiz-answer').off('click');
-          
-        }
-      });
-    }
-  }
-  var quiz = new Quiz();
-  quiz.init();
+function loadQ2(){
+  let quiz=document.querySelector('.quiz');
+  quiz.outerHTML =  `
+  <h2 class="quiz-question">
+  That's great to hear! wanna check out my website?</h2>
+  <input id="clickMe" type="button" value="Sure!" onclick="openWeb();" />
+  <input id="clickMe" type="button" value="No Thanks!" onclick="closeTab();" />
+  `
+}
+
+function loadQ3(){
+
+  let quiz=document.querySelector('.quiz');
+  quiz.outerHTML =  `<h2 class="quiz-question">That's good to hear, would you like to peep my website?</h2>
+    <input id="clickMe" type="button" value="Sure, I'm down!" onclick="openWeb();" />
+    <input id="clickMe" type="button" value="No thanks, I'm good for now!" onclick="closeTab();" />`
+}
+
+function loadQ4(){
+
+  let quiz=document.querySelector('.quiz');
+  quiz.outerHTML =  `<h2 class="quiz-question">Fair enough, would you like to see my website?</h2>
+    <input id="clickMe" type="button" value="Sure" onclick="openWeb();" />
+    <input id="clickMe" type="button" value="No" onclick="closeTab();" />`
+}
+
+function loadQ5(){
+
+  let quiz=document.querySelector('.quiz');
+  quiz.outerHTML =  `
+    <h2 class="quiz-question">I'm sorry to hear that mate, want a website to cheer you up?</h2>
+    <input id="clickMe" type="button" value="Yes please..." onclick="openWeb();" />
+    <input id="clickMe" type="button" value="Nah, I think I'll be fine" onclick="closeTab();" />`
+}
+
+//Found out that you can't nest load functions in javascript :(
+/*function loadQ6(){
+
+  let quiz=document.querySelector('.quiz');
+  quiz.outerHTML =  `<h2 class="quiz-question">Was that helpful?</h2>
+    <input id="clickMe" type="button" value="Yes it was" onclick="loadQ7();" />
+    <input id="clickMe" type="button" value="Not really..." onclick="loadQ4();" />`
+}
+
+function loadQ7(){
+
+  let quiz=document.querySelector('.quiz');
+  quiz.outerHTML =  `<h2 class="quiz-question">Happy to hear! Now another question, would you like to see my website?</h2>
+  <input id="clickMe" type="button" value="Sure sure" onclick="openWeb();" />
+  <input id="clickMe" type="button" value="No thanks" onclick="closeTab();" />`
+}*/
+
+function openWeb(){
+  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+function closeTab(){
+  window.open('', '_self', '');
+  window.close();
+}
+
+/*function showVid(){
+  console.log ('legos')
+}*/
